5aa50faf818e300d9873b7f8a531f7953ca272ed
Sithu <144105209+sithuAOAI@users.noreply.github.com>
2024-05-06 16:49:04 +0900

    Update __init__.py

--- api/getIceServerToken/__init__.py
+++ api/getIceServerToken/__init__.py
@@ -1,3 +1,41 @@
+# import logging
+# import os
+# import json
+# from azure.communication.networktraversal import CommunicationRelayClient
+# from azure.communication.identity import CommunicationIdentityClient
+
+# import azure.functions as func
+
+# connection_str = os.getenv("ICE_CONNECTION_STRING")
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+
+#     identity_client = CommunicationIdentityClient.from_connection_string(connection_str)
+#     relay_client = CommunicationRelayClient.from_connection_string(connection_str)
+
+#     _ = identity_client.create_user()
+#     relay_client.get_relay_configuration()
+
+#     relay_configuration = relay_client.get_relay_configuration()
+
+#     for iceServer in relay_configuration.ice_servers:
+#         assert iceServer.username is not None
+#         assert iceServer.credential is not None
+#         assert iceServer.urls is not None
+#         for url in iceServer.urls:
+#             print('Url:' + url)
+
+#         credentials = {
+#             "username": iceServer.username,
+#             "credential": iceServer.credential
+#         }
+
+#     return func.HttpResponse(
+#             json.dumps(credentials),
+#             status_code=200
+#     )
+
 import logging
 import os
 import json
@@ -9,29 +47,36 @@ import azure.functions as func
 connection_str = os.getenv("ICE_CONNECTION_STRING")
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    try:
+        logging.info('Python HTTP trigger function processed a request.')
 
-    identity_client = CommunicationIdentityClient.from_connection_string(connection_str)
-    relay_client = CommunicationRelayClient.from_connection_string(connection_str)
+        identity_client = CommunicationIdentityClient.from_connection_string(connection_str)
+        relay_client = CommunicationRelayClient.from_connection_string(connection_str)
 
-    _ = identity_client.create_user()
-    relay_client.get_relay_configuration()
+        _ = identity_client.create_user()
+        relay_client.get_relay_configuration()
 
-    relay_configuration = relay_client.get_relay_configuration()
+        relay_configuration = relay_client.get_relay_configuration()
 
-    for iceServer in relay_configuration.ice_servers:
-        assert iceServer.username is not None
-        assert iceServer.credential is not None
-        assert iceServer.urls is not None
-        for url in iceServer.urls:
-            print('Url:' + url)
+        for iceServer in relay_configuration.ice_servers:
+            assert iceServer.username is not None
+            assert iceServer.credential is not None
+            assert iceServer.urls is not None
+            for url in iceServer.urls:
+                print('Url:' + url)
 
-        credentials = {
-            "username": iceServer.username,
-            "credential": iceServer.credential
-        }
+            credentials = {
+                "username": iceServer.username,
+                "credential": iceServer.credential
+            }
 
-    return func.HttpResponse(
-            json.dumps(credentials),
-            status_code=200
-    )
+        return func.HttpResponse(
+                json.dumps(credentials),
+                status_code=200
+        )
+    except Exception as e:
+        logging.error(f'Error: {e}')
+        return func.HttpResponse(
+                "An error occurred.",
+                status_code=500
+        )
